50a7ccda905015d1a66f7234d6b8f3a1813403d8
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2023-01-18 10:00:31 -0300

    Instalação a Api inicial e configuração do mapa

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicial = {
+    latitude: -23.52608202998518,
+    longitude: -46.54024478809865,
+    latitudeDelta: 0.000000000009202,
+    longitudeDelta: 0.01421,
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <MapView style={styles.mapa} initialRegion={regiaoInicial} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  mapa: {
+    width: "100%",
+    height: "100%",
   },
 });
